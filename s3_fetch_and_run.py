e27a2a7b1a910d8fb655f31ca3adc6487633493c
mcmenemy <josh.mcmenemy@gmail.com>
2022-05-24 16:43:56 -0700

    take protein file as an input

--- s3_fetch_and_run.py
+++ s3_fetch_and_run.py
@@ -11,15 +11,19 @@ import boto3
 S3_BUCKET = 'openzyme'
 
 
-def fetch_and_run_script(input_s3_filename):
+def fetch_and_run_script(script_s3_filename, protein_s3_filename):
     s3 = boto3.client('s3')
     with tempfile.TemporaryDirectory() as tmpdirname:
-        input_script_filepath = f'{tmpdirname}/{input_s3_filename}'
-        input_s3_path = f'input_scripts/{input_s3_filename}'
-        s3.download_file(S3_BUCKET, input_s3_path, input_script_filepath)
+        protein_filepath = f'{tmpdirname}/{protein_s3_filename}'
+        s3.download_file(S3_BUCKET, protein_s3_filename, protein_filepath)
+
+        script_filepath = f'{tmpdirname}/{script_s3_filename}'
+        s3.download_file(S3_BUCKET, script_s3_filename, script_filepath)
+
         os.chdir(tmpdirname)
-        cmd = f'python {input_script_filepath}'
+        cmd = f'python {script_filepath}'
         os.system(cmd)
+
         output_filename = str(uuid.uuid4())
         shutil.make_archive(output_filename, 'zip', tmpdirname)
         output_s3_path = f'results/{output_filename}.zip'
@@ -28,6 +32,7 @@ def fetch_and_run_script(input_s3_filename):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_s3_file", type=str)
+    parser.add_argument("-s", "--script_s3_filename", type=str)
+    parser.add_argument("-p", "--protein_s3_filename", type=str)
     args = parser.parse_args()
-    fetch_and_run_script(args.input_s3_file)
+    fetch_and_run_script(args.script_s3_filename, args.protein_s3_filename)
